d7de05e4dfed51359322c69d1ee596fa845440f7
Drew Harris <harrisd@smu.edu>
2023-12-09 23:59:56 -0600

    Working!!!!
    New library

--- main.go
+++ main.go
@@ -1,63 +1,34 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"io"
 	"os"
 
-	shout "github.com/jdarnold/go-libshout"
+	"github.com/viert/shout"
 )
 
-// Setup some command line flags
-
 func main() {
-	fmt.Println("WE MADE IT DUDE!!")
-	flag.Parse()
+	f, err := os.Open("./ageis.mp3")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-	// Setup libshout parameters
-	s := shout.Shout{
+	cfg := &shout.Config{
 		Host:     "localhost",
 		Port:     8000,
 		User:     "source",
 		Password: "hackme",
-		Mount:    "test-mount",
-		Format:   shout.FORMAT_MP3,
-		Protocol: shout.PROTOCOL_HTTP,
+		Mount:    "/shuffle",
+		Proto:    shout.ProtocolHTTP,
+		Format:   shout.ShoutFormatMP3,
 	}
 
-	// Open the file
-	//
-	file, err := os.Open("ageis.mp3")
+	w, err := shout.Connect(cfg)
 	if err != nil {
 		panic(err)
 	}
+	defer w.Close()
 
-	// Create a channel where we can send the data
-	//
-	stream, err := s.Open()
-	if err != nil {
-		panic(err)
-	}
-	// /opt/homebrew/Cellar/libshout/2.4.6_1/include/shout/shout.h
-
-	defer s.Close()
-
-	buffer := make([]byte, shout.BUFFER_SIZE)
-	fmt.Println("BUFFER SIZE: ", shout.BUFFER_SIZE)
-	for {
-		// Read from file
-		n, err := file.Read(buffer)
-
-		if err != nil && err != io.EOF {
-			panic(err)
-		}
-
-		if n == 0 {
-			break
-		}
-
-		// Send to shoutcast server
-		stream <- buffer
-	}
+	io.Copy(w, f)
 }
